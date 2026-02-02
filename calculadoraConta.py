3e52bfd840759c9ddcba08bce6f0446d622e4ae9
HenriqueDML <hfgarcia97@gmail.com>
2026-02-01 23:44:31 -0300

    ajustando nova ideia

--- calculadoraConta.py
+++ calculadoraConta.py
@@ -4,7 +4,16 @@ input()
 print("--- Vamos pegar as informações da casa ---")
 input()
 
-qtd_pessoas = int(input("Digite a quantidade de pessoas na casa: "))
+qtd_morador = int(input("Digite quantas pessoas vão pagar a contas: "))
+lista_pagador = []
+qtd_pagador = lista_pagador
+while len(lista_pagador) < qtd_morador:
+    nome_morador = str(input("Digite o nome da primeira pessoa"))
+    lista_pagador.append(nome_morador)
+print(lista_pagador)
+
+"""
+#qtd_pessoas = numero de nomes na lista
 
 conta_luz = float(input("Digite aqui o valor da conta de Luz: "))
 conta_gas = float(input("Digite aqui o valor da conta de Gás: "))
@@ -16,4 +25,4 @@ conta_total = conta_luz + conta_gas + conta_internet + conta_aluguel + conta_con
 conta_dividida = conta_total / qtd_pessoas
 
 print(f"Esse é o valor total a ser pago: {conta_total:.2f}")
-print(f"Cada um deverá pagar: {conta_dividida:.2f}")
+print(f"Cada um deverá pagar: {conta_dividida:.2f}")"""
